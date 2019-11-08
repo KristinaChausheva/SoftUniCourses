7fa01c3ac2f071ad12cba85751da7ce16be09d9d
Kristina <k.chausheva@gmail.com>
2019-11-08 21:52:56 +0200

    JS

--- JS-Courses/JS-Basics/05.WhileLoopExercises/03.Vacation.js
+++ JS-Courses/JS-Basics/05.WhileLoopExercises/03.Vacation.js
@@ -1,43 +1,35 @@
 function vacation(params) {
-    const neededMoney = Number(params.shift());
+    let neededMoney = Number(params.shift());
     let totalSum = Number(params.shift());
 
-    let n = 0;
-    let action = "";
-    let sum = 0;
+    let na = 0;
     let daysCounter = 0;
 
-    while ((n < 5) && (totalSum < neededMoney)) {
+    while (totalSum < neededMoney) {
         daysCounter++;
-        action = params.shift();
-        sum = params.shift();
-        if (action == "spend") {
-            n++;
-            if (sum >= totalSum) {
+        let action = params.shift();
+        let sum = params.shift();
+
+        if (action === "spend") {
+            if (sum <= totalSum) {
+                totalSum -= sum;
+            } else {
                 totalSum = 0;
             }
-            else {
-                totalSum -= sum;
+            na++;
+            if (na === 5) {
+                break;
             }
-        }
-        else {
-            n = 0;
+        } else {
+            na = 0;
             totalSum += sum;
         }
-        
-    }
-    if (n == 5) {
-        console.log("You can't save the money.");
-        console.log(daysCounter);
-
     }
     if (totalSum >= neededMoney) {
         console.log(`You saved the money for ${daysCounter} days.`);
+    } else {
+        console.log("You can't save the money.");
+        console.log(daysCounter);
     }
 }
-
-//vacation([110, 60, "spend", 10, "save", 5, "spend", 5, "spend", 5, "spend", 5, "spend", 5, "spend", 5])
-//vacation([2000, 1000, "spend", 1200, "save", 2000])
 vacation([250,150,"spend", 50, "spend", 50, "save", 100, "save", 100])
-
-//vacation([110, 60, "spend", 10, "spend", 5, "spend", 5, "spend", 5, "spend", 5])
