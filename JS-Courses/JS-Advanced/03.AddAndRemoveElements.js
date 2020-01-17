6998ed4e8ba82239612e28fdfc747aaf225d96ca
Kristina Chausheva <k.chausheva@gmail.com>
2020-01-18 01:19:33 +0200

    updates

---
+++ JS-Courses/JS-Advanced/03.AddAndRemoveElements.js
@@ -0,0 +1,28 @@
+function solve(params) {
+    let initNum = 1;
+    let counter = params.length;
+    let command = "default";
+    let arr = [];
+    while (counter > 0) {
+        command = params.shift();
+        if (command === "add") {
+            arr.push(initNum);
+        } else {
+            arr.pop(initNum);
+        }
+        counter--;
+        initNum++;
+    }
+    if (arr.length === 0) {
+        console.log("Empty");
+        
+    } else {
+        for (const el of arr){
+            console.log(el);
+        }
+    }
+    
+   
+}
+
+solve(["remove", "remove", "remove"]);
