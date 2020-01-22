68a40e7d45717a45b47db1d49aec4e82b5bfc30d
Kristina Chausheva <k.chausheva@gmail.com>
2020-01-22 23:35:37 +0200

    updates

---
+++ JS-Courses/JS-Advanced/03.ObjectsExercises/03.CappyJuice.js
@@ -0,0 +1,45 @@
+function solve(params) {
+    const obj = {};
+    const orderedList = [];
+    let arr = [];
+    for (const i of params) {
+        //console.log(typeof(i));
+        arr = i.split(" => ")
+        //console.log(arr);
+
+        if (obj.hasOwnProperty(arr[0])) {
+            let currentKey = arr[0];
+            let currentValue = parseInt(arr[1]);
+            obj[currentKey] += currentValue;
+        } else {
+            obj[arr[0]] = parseInt(arr[1]);
+        }
+        for (const key in obj) {
+            
+            if (obj[key] / 1000 > 1) {
+                
+                let currentVal = Math.trunc((obj[key] / 1000));
+                orderedList.push(`${key} => ${currentVal}`);
+                
+                
+                
+            }
+        }
+       
+    }
+    //console.log(orderedList);
+    
+    console.log(obj);
+    
+}
+
+solve(
+    [
+        'Kiwi => 234',
+        'Pear => 2345',
+        'Watermelon => 3456',
+        'Kiwi => 4567',
+        'Pear => 5678',
+        'Watermelon => 6789'
+    ]
+)
---
+++ JS-Courses/JS-Advanced/doodles.js
@@ -0,0 +1,6 @@
+function name(params) {
+    console.log(params / 1000);
+    
+}
+
+name(2001)
