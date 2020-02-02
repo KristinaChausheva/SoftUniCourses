f4e5585fe33471694de157f04a75abf6d7ce4829
Kristina Chausheva <k.chausheva@gmail.com>
2020-02-02 18:37:59 +0200

    updates

--- JS-Courses/JS-Advanced/05.DOMExercises/02. Number-Convertor/solution.js
+++ JS-Courses/JS-Advanced/05.DOMExercises/02. Number-Convertor/solution.js
@@ -1,4 +1,24 @@
 function solve() {
+    let optionList = document.getElementById('selectMenuTo');
+    let binaryOption = document.createElement('option');
+    binaryOption.textContent = "binary";
+    let hexOption = document.createElement('option');
+    
+    hexOption.textContent = "hex";
+    optionList.appendChild(binaryOption);
+    document.getElementById("selectMenuTo").appendChild(hexOption);
 
-    //TODO...
+    let button = document.getElementsByTagName('button')[0];
+
+    let input = document.getElementById('input');
+    button.addEventListener('click', (e) => {
+        let result;
+        if (optionList.value === 'binary') {
+            result = (Number(input.value)).toString(2);
+            
+        } else {
+            result = (Number(input.value)).toString(16).toUpperCase();
+        }
+        document.getElementById('result').value = result;
+    })
 }
