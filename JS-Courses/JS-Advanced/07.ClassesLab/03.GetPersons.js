3c63697b72d043ceea6d31fe5e3342b04d020725
Kristina Chausheva <k.chausheva@gmail.com>
2020-02-06 06:22:45 +0200

    update

--- JS-Courses/JS-Advanced/07.ClassesLab/03.GetPersons.js
+++ JS-Courses/JS-Advanced/07.ClassesLab/03.GetPersons.js
@@ -11,10 +11,10 @@ function solve() {
             return `${this.firstName} ${this.lastName} (age: ${this.age}, email: ${this.email})`
         }
     }
-    let person1 = new Person('Anna', 'Simpson', 22, 'ana@yahoo.com');
+    let person1 = new Person('Anna', 'Simpson', 22, 'anna@yahoo.com');
     let person2 = new Person('SoftUni');
     let person3 = new Person('Stephan', 'Johnson', 25);
-    let person4 = new Person('Gabriel', 'Peterson', 24, 'g.pa@gmail.com');
+    let person4 = new Person('Gabriel', 'Peterson', 24, 'g.p@gmail.com');
     return [person1, person2, person3, person4]
 }
 
