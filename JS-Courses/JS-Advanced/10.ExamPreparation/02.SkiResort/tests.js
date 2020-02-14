d9f6ed7b23da4780867b9fea81bae1d78ec18686
Kristina Chausheva <k.chausheva@gmail.com>
2020-02-14 06:13:14 +0200

    updates

--- JS-Courses/JS-Advanced/10.ExamPreparation/02.SkiResort/tests.js
+++ JS-Courses/JS-Advanced/10.ExamPreparation/02.SkiResort/tests.js
@@ -1,5 +1,67 @@
 let SkiResort = require('./solution');
+let assert = require('chai').assert;
 
 describe('SkiResort', function () {
-    //TODO... =)
+
+    let actualResult;
+    let expectResult;
+
+    beforeEach(() => {
+        actualResult = null;
+        expectResult - null;
+    });
+
+    describe('constructor', () => {
+
+        it('check with no name arg', () => {
+            let obj = new SkiResort();
+            expectResult = undefined;
+            assert.equal(actualResult,expectResult,'')
+        });
+
+        it('bestHotel behavior() with no voters', () => {
+            let obj = new SkiResort();
+            actualResult = obj.bestHotel;
+            expectResult = 'No votes yet';
+            assert.equal(actualResult,expectResult);
+        });
+
+        it('bestHotel behavior() with voters', () => {
+            let obj = new SkiResort();
+            actualResult = obj.bestHotel;
+            expectResult = 'No votes yet';
+            assert.equal(actualResult,expectResult);
+        });
+
+
+        
+    });
+
+    describe('build() behavior',() => {
+        it('with no args', () => {
+            let obj = new SkiResort();
+            expectResult = 'Successfully built new hotel - undefined';
+            actualResult = obj.build();
+            assert.equal(actualResult,expectResult)
+        });
+        
+        it('with empty str name', () => {
+            let obj = new SkiResort();
+            expectResult = throw new Error;
+            actualResult = obj.build("");
+            assert.equal(actualResult,expectResult)
+        });
+    });
+    
+
+    
+    it('', () => {
+
+    });
+    it('', () => {
+
+    });
+    it('', () => {
+
+    });
 });
