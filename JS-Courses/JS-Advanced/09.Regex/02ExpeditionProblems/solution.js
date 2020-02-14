ef913ff4d6b89c375c349ede9b247762dabd0b9c
Kristina Chausheva <k.chausheva@gmail.com>
2020-02-14 05:07:45 +0200

    updates

--- JS-Courses/JS-Advanced/09.Regex/02ExpeditionProblems/solution.js
+++ JS-Courses/JS-Advanced/09.Regex/02ExpeditionProblems/solution.js
@@ -1,4 +1,40 @@
+// let key = '4ds';
+// let message = 'eaSt 19,432567noRt north east 53,123456north 43,3213454dsNot all those who wander are lost.4dsnorth 47,874532'
+
 function solve() {
 
-    //TODO...
+    let key = document.getElementById('string').value;
+    let message = document.getElementById('text').value;
+    let resultRef = document.getElementById('result');
+    let saveMessage = message.match(`${key}(.+)${key}`)[1];
+    let ourRegex = new RegExp(/(?<direction>east|north).*?(?<coordinates>\d{2}).*?,[^,].*?(?<decimals>\d{6})/gmi)
+    
+    let north;// = message.match(ourRegex).filter(x =>x.match(/^north/ig));
+    let east;// = message.match(ourRegex).filter(x => x.match(/^east/ig));
+    let temp = ourRegex.exec(message);
+    while (temp) {
+        if (temp[1].toLowerCase()==='east') {
+            east = `${temp[2]}.${temp[3]}`;
+        } else {
+            north = `${temp[2]}.${temp[3]}`;
+        }
+        temp = ourRegex.exec(message);
+    }
+    // console.log(saveMessage);
+    
+    // console.log(east);
+    // console.log(north);
+
+    // let lastNorthElement = north.pop().split(' ').pop(',');
+    // let lastEastElement = east.pop().split(' ').pop(',');
+
+    resultRef.innerHTML =  `
+    <p>${north} N</p>
+    <p>${east} E</p>
+    <p>Message: ${saveMessage}</p>
+    `;
+    
+
 }
+
+// solve()
--- JS-Courses/JS-Advanced/09.Regex/03JamesBond/solution.js
+++ JS-Courses/JS-Advanced/09.Regex/03JamesBond/solution.js
@@ -1,4 +1,30 @@
+let key = 'specialKey';
+let line = 'In this text the specialKey HELLOWORLD! is correct, but the following specialKey $HelloWorl#d and spEcIaLKEy HOLLOWORLD1 are not, while SpeCIaLkeY SOM%%ETH$IN and SPECIALKEY ##$$##$$ are!]'
+
 function solve() {
 
-    //TODO...
+    // let [key, ...line] = JSON.parse(document.getElementById('array').value);
+    // let resultRef = document.getElementById('result');
+    let ourRegex = new RegExp(`${key}[ ] +([A-Z!%#\$]{8, })([\., ]|$)`,'gmi');
+    line.forEach(dataLine => {
+        let replacedLine = dataLine;
+        if (dataLine.match(ourREgex)) {
+            let matches = dataLine.match(ourRegex)
+            .map(x => x.split(" ")[1])
+            .filter(str => str === str.toUpperCase('p'))
+            .map(x => {
+
+            })
+        }
+
+        
+        
+        // let tempP = document.createElement('p');
+        // tempP.innerText = replacedLine;
+        // resultRef.appendChild(tempP);
+        //line.match(dataLine.ourRegex[1]);
+    });
+    console.log(ourRegex);
 }
+
+solve()
