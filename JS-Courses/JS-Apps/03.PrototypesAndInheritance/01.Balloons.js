af878b336b14de69b1ba379a07f5c69f4844a217
Kristina Chausheva <k.chausheva@gmail.com>
2020-03-07 18:43:55 +0200

    updates

--- JS-Courses/JS-Apps/03.PrototypesAndInheritance/01.Balloons.js
+++ JS-Courses/JS-Apps/03.PrototypesAndInheritance/01.Balloons.js
@@ -10,31 +10,27 @@ function solve() {
     class PartyBalloon extends Balloon {
         constructor(color,gasWeight, ribbonColor,ribbonLenght) {
             super(color, gasWeight);
-            this.ribbon = {ribbonColor,ribbonLenght};
+            this._ribbon = {
+                color: ribbonColor,
+                length: ribbonLenght
+            };
         }
 
         get ribbon() {
-            return this.ribbon;
-        }
-
-        set ribbon({ribbonColor, ribbonLenght}){
-            return { ribbonColor, ribbonLenght}
+            return this._ribbon;
         }
     }
 
     class BirthdayBalloon extends PartyBalloon {
         constructor(color,gasWeight, ribbonColor,ribbonLenght, text) {
             super(color, gasWeight, ribbonColor,ribbonLenght);
-            this.text = text;
+            this._text = text;
         }
 
         get text(){
-            return this.text;
+            return this._text;
         }
-        
-        set text(text) {
-            return text;
-        }
+       
     }
 
     return {
