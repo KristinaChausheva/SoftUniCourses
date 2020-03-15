de44ebc8ac69780842f18aec0d54197e718ba264
Kristina Chausheva <k.chausheva@gmail.com>
2020-03-15 14:52:57 +0200

    updates

--- JS-Courses/JS-Apps/05/01. FORECASTER/app.js
+++ JS-Courses/JS-Apps/05/01. FORECASTER/app.js
@@ -1,5 +1,35 @@
 function attachEvents() {
-    console.log("TODO...");
+
+    const BASE_URL = 'https://judgetests.firebaseio.com/locations.json';
+
+    const elements = {
+        locationInput: document.querySelector('#location'),
+        button: document.querySelector('#submit'),
+        notificationHeading: document.querySelector('h1.notification')
+    };
+
+    const errHandler = (e) => {
+        console.dir(e);
+        elements.notificationHeading.textContent = e.message;
+        
+    }
+
+    elements.button.addEventListener('click', getLocationValue);
+
+    function getLocationValue() {
+        const location = elements.locationInput.value;
+        fetch(BASE_URL)
+        .then((r) => r.json())
+        .then((d) => {
+            const locationData = d.find((o) => o.name === location);
+            console.log(locationData);
+            
+        })
+        .catch(errHandler)
+    }
+
+
+
 }
 
 attachEvents();
