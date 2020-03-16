d5ba95ea2092a82c2c496beb767c544d98f1eef5
Kristina Chausheva <k.chausheva@gmail.com>
2020-03-16 15:20:12 +0200

    updates

---
+++ JS-Courses/JS-Apps/05.AsynchronousProgramming/01. FORECASTER/app.js
@@ -0,0 +1,45 @@
+function attachEvents() {
+
+    let BASE_URL = 'https://judgetests.firebaseio.com/locations.json';
+    let CURRENT_WEATHER_URL = `https://judgetests.firebaseio.com/forecast/today/code.json`;
+
+    const elements = {
+        locationInput: document.querySelector('#location'),
+        button: document.querySelector('#submit'),
+        notificationHeading: document.querySelector('h1.notification')
+    };
+
+    const errHandler = (e) => {
+        console.dir(e);
+        elements.notificationHeading.textContent = e.message;
+        
+    }
+
+    elements.button.addEventListener('click', getLocationValue);
+
+    function getLocationValue() {
+        const location = elements.locationInput.value;
+        fetch(BASE_URL)
+        .then((r) => r.json())
+        .then((d) => {
+            const locationData = d.find((o) => o.name === location);
+            console.log(locationData.code);
+            
+        })
+        .catch(errHandler);
+
+        fetch(CURRENT_WEATHER_URL)
+        .then((r) => r.json())
+        .then((d) => {
+            const currWeather = d.find((o) => o.name === locationData.code);
+            console.log(currWeather);
+            
+        })
+        .catch((e) => console.log(e.message));
+    }
+
+
+
+}
+
+attachEvents();
