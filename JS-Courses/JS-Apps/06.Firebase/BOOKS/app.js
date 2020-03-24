476a5c72c96963e207bde59be7fbdddbb50d912b
Kristina Chausheva <k.chausheva@gmail.com>
2020-03-25 00:24:18 +0200

    updates

--- JS-Courses/JS-Apps/06.Firebase/BOOKS/app.js
+++ JS-Courses/JS-Apps/06.Firebase/BOOKS/app.js
@@ -1,12 +1,29 @@
-import { apiKey} from './firebase-requests.js';
+import { apiKey, createNewBook } from './firebase-requests.js';
 
-function doStuff(){
+function extractFormData (formRef, formConfig) {
+    return formConfig.reduce((acc, inputName) => {
+        acc[inputName] = formRef.elements[inputName].value;
+        return acc;
+    }, {})
+}
+
+function doStuff() {
+
+    let formInputs = ['title', 'author', 'isbn'];
 
     let formRef = document.querySelector('form');
 
+    formRef.addEventListener('submit', (e) => {
+        e.preventDefault();
+
+        let parsedForm = extractFormData(formRef, formInputs)
+        createNewBook(parsedForm);
+
+
+    })
 
     console.log(apiKey);
-    
+
 }
 
 doStuff();
